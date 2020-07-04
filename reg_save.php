c9fcff5854cb60b1c8d960a03dc3ec315a70c5f9
Timicasto <67822084+Timicasto@users.noreply.github.com>
2020-07-04 16:51:07 +0800

    Write registration information to MySQL

--- reg_save.php
+++ reg_save.php
@@ -1,13 +1,11 @@
 ﻿<?php
 include('./link.php');
 
-//第一步：接收数据
 $username = $_POST['username'];
 $password = $_POST['password'];
 $password2 = $_POST['password2'];
 $email = $_POST['email'];
 
-//第二步：验证数据有效性
 if($password!=$password2){
     echo '两次输入的密码不一致！请重新输入！';exit;
 }
@@ -20,21 +18,19 @@ if( strlen($password)<6 ){
 $time = $showtime=date("Y-m-d H:i:s");
 $password = md5($password);
 
-//第三步：构造SQL语句，实现注册用户功能（向member数据表插入一条数据）
 $sql = "INSERT INTO member(username, regtime, password, email) VALUES ('$username', '$time', '$password', '$email')";
 $r = mysqli_query($link, $sql);
 
-//第四步：结果处理
 if($r){
     /* 使用SESSION */
     //SESSION-注册成功后自动登录
-    $user_id = mysqli_insert_id($link); //获取最近一条插入进数据库的数据的ID
+    $user_id = mysqli_insert_id($link); 
     session_start();
     $_SESSION['username'] = $username;
     $_SESSION['user_id'] = $user_id;
     $_SESSION['email'] = $email;
 
-    echo '恭喜你，注册成功！<a href="./new.php">返回发帖</a>';
+    echo '恭喜你，注册成功！<a href="ui/main.html">返回主页</a>';
 }else{
-    echo '注册失败，请再来一次或联系管理员！<a href="./new.php">返回发帖</a>';
+    echo '注册失败，请再来一次或联系管理员！<a href="ui/main.html">返回发帖</a>';
 }
