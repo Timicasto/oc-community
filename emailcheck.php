8292389c6529230be14915cf9f8fa19d39a1703b
Timicasto <67822084+Timicasto@users.noreply.github.com>
2020-07-04 16:46:24 +0800

    [Temporarily unavailable]Send verification code to check email during registration

--- emailcheck.php
+++ emailcheck.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('log_errors', 'on');
-ini_set('error_log', 'syslog');
-error_reporting(-1);
-$token = rand(10000, 99999);
-echo $token;
 $email = $_POST['email'];
-mail("$email","测试","测试邮件");
+mail("$email","title","message");
+?>
