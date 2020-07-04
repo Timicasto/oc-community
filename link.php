8829061f11c5e5e9628f33b9ad83411fae3ff9c3
Timicasto <67822084+Timicasto@users.noreply.github.com>
2020-07-04 16:44:06 +0800

    Connect to MySQL server

--- link.php
+++ link.php
@@ -1,5 +1,5 @@
 ﻿<?php
 header('Content-Type:text/html; charset=utf-8');
 
-$link = @mysqli_connect('database.idatac.com', 'oc', 'cf286b25a022976f6bd75a5be7b8a442ce0b078d','oc') or die('connect error!');
+$link = @mysqli_connect('sql-address', 'username', 'passwd','database') or die('connect error!');
 mysqli_query($link, 'set names utf8');
